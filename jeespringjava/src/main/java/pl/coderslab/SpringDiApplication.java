6d147922c4cf41a774e4439db08fded518f4df3d
Kamil Zur <kamil.zur@ingservicespolska.pl>
2019-05-25 16:36:14 +0200

    Tasks 2,3,4

--- jeespringjava/src/main/java/pl/coderslab/SpringDiApplication.java
+++ jeespringjava/src/main/java/pl/coderslab/SpringDiApplication.java
@@ -2,14 +2,33 @@ package pl.coderslab;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import pl.coderslab.beans.HelloWorld;
+import pl.coderslab.beans.ScopePrototype;
+import pl.coderslab.beans.ScopeSingleton;
+import pl.coderslab.beans.Ship;
 import pl.coderslab.config.Config;
 
 public class SpringDiApplication {
 
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
-        HelloWorld helloWorld = context.getBean("helloWorld", HelloWorld.class);
+        //Zadanie 2
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(Config.class);
+        HelloWorld helloWorld = ctx.getBean("helloWorld", HelloWorld.class);
         helloWorld.hello();
-        context.close();
+        //Zadanie 3
+        Ship ship = ctx.getBean("blackPearl", Ship.class);
+        ship.getCaptain().startSailing();
+        //Zadanie 4
+        ScopeSingleton singleton1 = ctx.getBean(ScopeSingleton.class);
+        System.out.println(singleton1);
+        ScopeSingleton singleton2 = ctx.getBean(ScopeSingleton.class);
+        System.out.println(singleton2);
+        System.out.println((singleton1 == singleton2) + ": ten sam obiekt\n");
+        ScopePrototype prototype1 = ctx.getBean(ScopePrototype.class);
+        System.out.println(prototype1);
+        ScopePrototype prototype2 = ctx.getBean(ScopePrototype.class);
+        System.out.println(prototype2);
+        System.out.println((prototype1 == prototype2) + ": dwa różne obiekty");
+
+        ctx.close();
     }
 }
--- jeespringjava/src/main/java/pl/coderslab/config/Config.java
+++ jeespringjava/src/main/java/pl/coderslab/config/Config.java
@@ -2,7 +2,8 @@ package pl.coderslab.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import pl.coderslab.beans.HelloWorld;
+import org.springframework.context.annotation.Scope;
+import pl.coderslab.beans.*;
 
 @Configuration
 public class Config {
@@ -10,5 +11,27 @@ public class Config {
     @Bean
     public HelloWorld helloWorld(){
         return new HelloWorld();
+    }
+
+    @Bean
+    public Captain jackSparrow() {
+        return new Captain();
+    }
+
+    @Bean
+    public Ship blackPearl() {
+        return new Ship(jackSparrow());
+    }
+
+    @Bean
+    @Scope("prototype")
+    public ScopePrototype scopePrototype(){
+        return new ScopePrototype();
+    }
+
+    @Bean
+    @Scope("singleton")
+    public ScopeSingleton scopeSingleton(){
+        return new ScopeSingleton();
     }
 }
